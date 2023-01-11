55ce803eec76900c7086cd84e9c8b320b7aacc92
tbrown034 <tbrown034@gmail.com>
2023-01-11 00:27:39 -0600

    better header formatting

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,7 +10,11 @@ import {
   MDBBtn,
   MDBIcon,
   MDBNavbarNav,
-  MDBInputGroup
+  MDBInputGroup,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
 } from 'mdb-react-ui-kit';
 
 export default function App() {
@@ -18,9 +22,9 @@ export default function App() {
 
   return (
     <>
-      <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBNavbar expand='md' light bgColor='light'>
         <MDBContainer fluid>
-          <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
+          <MDBNavbarBrand href='#'>Home</MDBNavbarBrand>
           <MDBNavbarToggler
             type='button'
             data-target='#navbarTogglerDemo02'
@@ -34,23 +38,50 @@ export default function App() {
           <MDBCollapse navbar show={showNavNoTogglerSecond}>
             <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
               <MDBNavbarItem>
-                <MDBNavbarLink active aria-current='page' href='#'>
-                  Home
+                <MDBNavbarLink href='#'>
+                  About
                 </MDBNavbarLink>
               </MDBNavbarItem>
               <MDBNavbarItem>
-                <MDBNavbarLink href='#'>Link</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-                  Disabled
+                <MDBNavbarLink href='#'>
+                  Financing
                 </MDBNavbarLink>
               </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>
+                  Location
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>
+                  Contact
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+              <MDBDropdown>
+                <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+                  Services
+                </MDBDropdownToggle>
+                <MDBDropdownMenu>
+                  <MDBDropdownItem link>General Dentristry</MDBDropdownItem>
+                  <MDBDropdownItem link>Emergency Dentristry</MDBDropdownItem>
+                  <MDBDropdownItem link>Cosmetic Dentristry </MDBDropdownItem>
+                  <MDBDropdownItem link>Childrens' Dentristry </MDBDropdownItem>
+                  <MDBDropdownItem link>Dental Crowns</MDBDropdownItem>
+                  <MDBDropdownItem link>Dental Implants </MDBDropdownItem>
+                  <MDBDropdownItem link>Whitening</MDBDropdownItem>
+                  <MDBDropdownItem link>More</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+            </MDBNavbarItem>
+
             </MDBNavbarNav>
-            <MDBInputGroup tag="form" className='d-flex w-auto mb-3'>
-              <input className='form-control' placeholder="Type query" aria-label="Search" type='Search' />
-              <MDBBtn outline>Search</MDBBtn>
-            </MDBInputGroup>
+         
+        <MDBBtn outline color="success" className='me-2' type='button'>
+          Make an Appointment
+        </MDBBtn>
+        
+   
           </MDBCollapse>
         </MDBContainer>
       </MDBNavbar>
