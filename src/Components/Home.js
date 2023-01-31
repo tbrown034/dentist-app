5fce671be48d63e8aad5341aa910001c97cb5358
tbrown034 <tbrown034@gmail.com>
2023-01-31 17:29:05 -0600

    better home formating, buttons, ect

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -34,7 +34,7 @@ const Home = () => {
             <div id="buttonContainer">
               <br></br>
               <a
-                className="btn btn-outline-light btn-lg"
+                className="btn btn-outline-light btn-lg rounded-pill"
                 href="/Appointment"
                 role="button"
               >
@@ -42,7 +42,7 @@ const Home = () => {
               </a>
               <br />
               <a
-                className="btn btn-outline-light btn-lg"
+                className="btn btn-outline-light btn-lg rounded-pill"
                 href="/FAQs"
                 role="button"
               >
