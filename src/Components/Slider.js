6107bf9eb4753b4237892dd5383e138e4381be21
tbrown034 <tbrown034@gmail.com>
2023-02-15 15:29:20 -0600

    better slider formating

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -3,7 +3,6 @@ import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { MDBIcon, MDBTypography } from "mdb-react-ui-kit";
 
-
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
@@ -12,54 +11,118 @@ import "swiper/css/pagination";
 import "./styles.css";
 
 // import required modules
-import { Navigation, Pagination } from "swiper";
+import { Navigation, Pagination, Autoplay } from "swiper";
 
 export default function Slider1() {
   return (
     <>
-      <Swiper navigation={true} pagination={{
-          type: "progressbar"
-        }} speed={1400} modules={[Navigation, Pagination]} className="mySwiper" >
-        <SwiperSlide><div>
-          <p>
-            <MDBIcon fas icon="quote-left" />
-            I have been receiving dental care from Dr. Brown for 17 years. I
-            truly believe he is the best in the business. He is a perfectionist
-            in his work. I highly recommend him and his staff.
-            <MDBIcon fas icon="quote-right" />
-            <MDBTypography 
-            listUnStyled
-            className="d-flex justify-content-center mb-0"
-          >
-            <li>
-              <MDBIcon fas icon="star" size="sm" className="text-warning" />
-            </li>
-            <li>
-              <MDBIcon fas icon="star" size="sm" className="text-warning" />
-            </li>
-            <li>
-              <MDBIcon fas icon="star" size="sm" className="text-warning" />
-            </li>
-            <li>
-              <MDBIcon fas icon="star" size="sm" className="text-warning" />
-            </li>
-            <li>
-              <MDBIcon far icon="star" size="sm" className="text-warning" />
-            </li>
-          </MDBTypography>
-          </p>
-         
-          <h4> - Mike L.</h4>
-          <h5>Napeville, IL</h5>
-        </div></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+      <Swiper
+        autoplay={{
+          delay: 4500,
+        }}
+        navigation={true}
+        pagination={{
+          type: "progressbar",
+        }}
+        speed={1400}
+        modules={[Navigation, Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <p>
+              <MDBIcon fas icon="quote-left" />
+              I have been receiving dental care from Dr. Brown for 17 years. I
+              truly believe he is the best in the business. He is a
+              perfectionist in his work. I highly recommend him and his staff.
+              <MDBIcon fas icon="quote-right" />
+              <MDBTypography
+                listUnStyled
+                className="d-flex justify-content-center mb-0"
+              >
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon far icon="star" size="sm" className="text-warning" />
+                </li>
+              </MDBTypography>
+            </p>
+
+            <h4> - Mike L.</h4>
+            <h5>Napeville, IL</h5>
+        </SwiperSlide>
+        <SwiperSlide><p>
+              <MDBIcon fas icon="quote-left" />
+              I initially came here with a dental crisis and needed an emergency
+            appointment. It's out of network for me but they did everything they
+            could with the insurance and I've gotten excellent care. My mouth
+            was in horrible shape and now it's a lot better. Instead of losing
+            some teeth I have teeth that look really good.
+              <MDBIcon fas icon="quote-right" />
+              <MDBTypography
+                listUnStyled
+                className="d-flex justify-content-center mb-0"
+              >
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon far icon="star" size="sm" className="text-warning" />
+                </li>
+              </MDBTypography>
+            </p>
+
+            <h4>- Mike C..</h4>
+            <h5>Markham, IL</h5></SwiperSlide>
+        <SwiperSlide><p>
+              <MDBIcon fas icon="quote-left" />
+              Dr. Brown made me a total new set of dentures that attaches to mini
+            implants. Now, I can eat sandwiches, grilled meats, BBQ ribs,
+            steaks, and all kinds of hard foods. If you are having problems with
+            your dentures, I would recommended Dr. Keith Brown to anyone.
+              <MDBIcon fas icon="quote-right" />
+              <MDBTypography
+                listUnStyled
+                className="d-flex justify-content-center mb-0"
+              >
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                </li>
+                <li>
+                  <MDBIcon far icon="star" size="sm" className="text-warning" />
+                </li>
+              </MDBTypography>
+            </p>
+
+            <h4>- Mattie P W.</h4>
+            <h5>Bolingbrook, ILL</h5></SwiperSlide>
       </Swiper>
     </>
   );
