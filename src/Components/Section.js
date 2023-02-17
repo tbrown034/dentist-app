821b1c9e85b0e6e81332c98a12fb6af83de0d88d
tbrown034 <tbrown034@gmail.com>
2023-02-17 15:08:42 -0600

    section optimized

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -1,79 +1,51 @@
-import { Segment } from "semantic-ui-react";
-import { Icon } from "semantic-ui-react";
+import { Segment, Icon } from "semantic-ui-react";
+
 const Section = () => {
-    return (
-        <>
-         <div id="sectionHolder">
-        <Segment.Group horizontal id="segmentGroup">
-          <Segment id="segment">
-            <Icon name="user md" size="big" />
-            <p>40+ Years of Experience</p>
-            <a
-              className="btn btn-outline-light btn-lg rounded-pill"
-              href="/Appointment"
-              role="button"
-            >
-              Meet Dr. Brown, D.D.S., F.A.G.D. <i className="arrow "></i>{" "}
-              <i
-                className="arrow alternate circle right
-icon"
-              ></i>
-            </a>
-          </Segment>
-          <Segment id="segment">
-            <Icon name="emergency" size="big" />
-            <p>24/7 Emergency Services</p>
-            <a
-              className="btn btn-outline-light btn-lg rounded-pill"
-              href="/Appointment"
-              role="button"
-            >
-              Emergnecy? Call Now <i className="arrow "></i>{" "}
-              <i
-                className="arrow alternate circle right
-icon"
-              ></i>
-            </a>
-          </Segment>
-          <Segment id="segment">
-            <Icon name="money bill alternate outline" size="big" />
-            <p>Affordable Care</p>
-            <a
-              className="btn btn-outline-light btn-lg rounded-pill"
-              href="/Appointment"
-              role="button"
-            >
-              Check Your Financing Options <i className="arrow "></i>{" "}
-              <i
-                className="arrow alternate circle right
-icon"
-              ></i>
-            </a>
-          </Segment>
-          <Segment id="segment">
-            <Icon name="heartbeat" size="big" />
-            <p>State of the Art Facilites</p>
-            <a
-              className="btn btn-outline-light btn-lg rounded-pill"
-              href="/Appointment"
-              role="button"
-            >
-              Take a Tour of our Office<i className="arrow "></i>{" "}
-              <i
-                className="arrow alternate circle right
-icon"
-              ></i>
-            </a>
-          </Segment>
-        </Segment.Group>
-      </div>
-        
-        
-        
-        </>
-    )
-}
+  const data = [
+    {
+      id: 1,
+      icon: "user md",
+      title: "40+ Years of Experience",
+      buttonText: "Meet Dr. Brown, D.D.S., F.A.G.D.",
+    },
+    {
+      id: 2,
+      icon: "emergency",
+      title: "24/7 Emergency Services",
+      buttonText: "Emergency? Call Now",
+    },
+    {
+      id: 3,
+      icon: "money bill alternate outline",
+      title: "Affordable Care",
+      buttonText: "Check Your Financing Options",
+    },
+    {
+      id: 4,
+      icon: "heartbeat",
+      title: "State of the Art Facilities",
+      buttonText: "Take a Tour of our Office",
+    },
+  ];
+
+  return (
+    <Segment.Group horizontal id="segmentGroup">
+      {data.map((item) => (
+        <Segment key={item.id} id="segment">
+          <Icon name={item.icon} size="big" />
+          <p>{item.title}</p>
+          <a
+            className="btn btn-outline-light btn-lg rounded-pill"
+            href="/Appointment"
+            role="button"
+          >
+            {item.buttonText} <i className="arrow "></i>{" "}
+            <i className="arrow alternate circle right icon"></i>
+          </a>
+        </Segment>
+      ))}
+    </Segment.Group>
+  );
+};
 
 export default Section;
-
-
