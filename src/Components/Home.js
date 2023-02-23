7c481899320bf3eb5c1491d2789d1aa838e7a097
tbrown034 <tbrown034@gmail.com>
2023-02-23 17:57:09 -0600

    more home changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,62 +2,55 @@ import React from "react";
 import Section from "./Section";
 import Slider1 from "./Slider";
 import "../index.css";
-import homePhoto from "./Images/dentist.jpeg"
+import homePhoto from "./Images/dentist.jpeg";
 import Promotions from "./Promotions";
 import Map from "./Map";
-import ServiceHome from "./ServiceHome"
+import ServiceHome from "./ServiceHome";
 
 const HeroStyle = {
   backgroundImage: `url(${homePhoto})`,
   backgroundColor: "rgba(0, 0, 0, 0.6)",
-  height: 550,
+  height: 650,
 };
-
 
 const Home = () => {
   return (
-    <main id="home">
-      <header className="img-fluid bg-image" style={HeroStyle}>
+    <main>
+      <div className="img-fluid bg-image" style={HeroStyle}>
         <div className="mask">
           <div id="homeContainer">
-            <div id="textContainer">
-              <h1 id="topHeroText">Let us brighten your smile.</h1>
+            <div id="texxtContainer">
+              <h1 id="topHeroText">Let us brigten your smile. </h1>
               <h3 id="bottomHeroText">
                 Dr. Keith A. Brown has been serving Napeville and the
                 Chicagoland area for more than 40 years. Schedule a cleaning or
                 appointment today!
               </h3>
-              <br />
-            </div>
-            <div id="buttonContainer">
-              <br />
-              <a
-                className="btn btn-outline-light btn-lg rounded-pill"
-                href="/Appointment"
-                role="button"
-              >
-                Make an Appointment
-              </a>
-              <a
-                className="btn btn-outline-light btn-lg rounded-pill"
-                href="/Appointment"
-                role="button"
-              >
-                Call (630) 396-8702
-              </a>
-             
-              <br />
+              <div id="buttonContainer">
+                <a
+                  className="btn btn-outline-light btn-lg rounded-pill"
+                  href="/Appointment"
+                  role="button"
+                >
+                  Make an Appointment
+                </a>
+                <a
+                  className="btn btn-outline-light btn-lg rounded-pill"
+                  href="/Appointment"
+                  role="button"
+                >
+                  Call Us At (630) 396-8702
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </header>
+      </div>
       <Section />
       <Slider1 />
-      <ServiceHome/>
-
+      <ServiceHome />
       <Promotions />
       <Map />
-
     </main>
   );
 };
